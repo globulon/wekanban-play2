ee2163e1df10b6c055742f3a2721c1b232b6252b
Marc-Daniel Ortega <globulon@gmail.com>
2012-03-09 17:17:12 +0100

    create story, and marshalling

---
+++ app/models/DomainMarshallers.scala
@@ -0,0 +1,10 @@
+package models
+
+/**
+ * Date: 09/03/12
+ * Time: 17:07
+ */
+
+class DomainMarshallers {
+
+}
---
+++ app/models/Story.scala
@@ -0,0 +1,10 @@
+package models
+
+/**
+ * Date: 09/03/12
+ * Time: 17:05
+ */
+
+class Story {
+
+}
